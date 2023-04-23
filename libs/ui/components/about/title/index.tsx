ba0bb244eaa17c4a42fcadc92e4b30aa0a7dde5e
aidenpwnz <aidenpwnz@proton.me>
2023-04-23 13:57:08 +0200

    bump

--- libs/ui/components/about/title/index.tsx
+++ libs/ui/components/about/title/index.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 export const AboutTitle: React.FC = () => (
-  <h3 className='text-xl text-blue-haze tracking-[20px] lg:absolute lg:top-[10%]'>
-    ABOUT
+  <h3 className='text-xl text-blue-haze tracking-[20px] lg:absolute lg:top-[10%] uppercase'>
+    About
   </h3>
 );
